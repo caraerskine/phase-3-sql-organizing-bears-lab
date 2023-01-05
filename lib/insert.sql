abb468315a8b77e54a6709cb8eb545629407c377
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-01-05 18:10:08 -0500

    commit tree 1acc3a363f95b0fe56f2005b80006dd2cc3d319c

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 1, 'M', 'Brown', 'Happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 2, 'F', 'Brown', 'Hungry', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 3, 'M', 'Black', 'Tired', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 1, 'F', 'White', 'Bored', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 5, 'M', 'Black', 'Silly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 9, 'F', 'Brown', 'Loud', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 12, 'F', 'Brown', 'Quiet', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (null, 10, 'M', 'Brown', 'Slow', 0); 
